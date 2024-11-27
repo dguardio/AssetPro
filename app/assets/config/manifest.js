6f3ba9e058fb75ca385bff337b4d4a2f40192854
Opeyemi Akinwoleola <akinwoleolaopeyemi@gmail.com>
2024-11-27 17:46:25 -0600

    Using the correct manifest statements to fix sprokets issue

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,4 @@
 //= link_tree ../images
-//= link_tree ../stylesheets/application.scss
 //= link_directory ../stylesheets .scss
-//= link_directory ../stylesheets/material-dashboard .css
-//= link_tree ../javascripts/material-dashboard .js
+//= link_tree ../javascripts .js
 //= link_tree ../builds
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
+// Add any additional JavaScript libraries or custom scripts here 
