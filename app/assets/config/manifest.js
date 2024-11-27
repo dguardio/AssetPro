0130da9e388cf8a930738008569362d00b9c4303
Opeyemi Akinwoleola <akinwoleolaopeyemi@gmail.com>
2024-11-27 15:29:03 -0600

    Sprockets Error fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link_tree ../../javascript .js
+//= link_tree ../../javascript/material-dashboard .js
 //= link_tree ../builds
+//= link application.scss
