caca7a39d8a4f3c249216c49f61333ba8ce0c157
Opeyemi Akinwoleola <akinwoleolaopeyemi@gmail.com>
2024-11-27 18:30:44 -0600

    Fix for double declaration of iconNavBar

--- app/assets/javascripts/application.js
+++
@@ -1,7 +0,0 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-
-//= require rails-ujs
-//= require_tree .
-
-// Add any additional JavaScript libraries or custom scripts here 
