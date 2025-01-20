3f0160e6586f893b73ae5c9104eec26a11caaed0
Opeyemi Akinwoleola <akinwoleolaopeyemi@gmail.com>
2025-01-20 15:04:51 -0600

    Remove auth from check-version endpoint

--- app/controllers/api/v1/updates_controller.rb
+++ app/controllers/api/v1/updates_controller.rb
@@ -4,7 +4,6 @@ module Api
     class UpdatesController < ApplicationController
       skip_before_action :verify_authenticity_token
       skip_before_action :authenticate_user!
-      skip_before_action :authenticate_user_from_token!
 
 
       def check_version
